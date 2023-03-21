d10ad5295dd9ec4ba0e195caa4de5dddc0897ad4
WimHambrouck-EhB <wim.hambrouck@ehb.be>
2023-03-21 18:58:22 +0100

    Add project files.

---
+++ ASPNetCore_EF_Students/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNetCore_EF_Students.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
